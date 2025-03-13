0e64921ab279ff0e3e81a605f6bb252efa8c6482
Melissa Weber Mendonça <melissawm@gmail.com>
2025-03-13 20:29:12 +0000

    Initial setup

---
+++ unconference/conf.py
@@ -0,0 +1,80 @@
+import os
+
+author = "NumFOCUS DISC Unconference 2025 attendees. Content is licensed under CC BY-SA 4.0 unless otherwise specified."
+comments_config = {"hypothesis": False, "utterances": False}
+exclude_patterns = ["**.ipynb_checkpoints", ".DS_Store", "Thumbs.db", "_build"]
+extensions = [
+    "sphinx_togglebutton",
+    "sphinx_copybutton",
+    "myst_nb",
+    "jupyter_book",
+    "sphinx_thebe",
+    "sphinx_comments",
+    "sphinx_external_toc",
+    "sphinx.ext.intersphinx",
+    "sphinx_design",
+    "sphinx_book_theme",
+    "sphinx_jupyterbook_latex",
+]
+language = os.environ.get("WEBSITE_LANGUAGE", "en")
+locale_dirs = ["../locales"]
+gettext_uuid = True
+gettext_compact = False
+external_toc_exclude_missing = False
+external_toc_path = "_toc.yml"
+html_baseurl = ""
+html_favicon = ""
+html_sourcelink_suffix = ""
+html_theme = "sphinx_book_theme"
+html_show_copyright = False
+html_static_path = ["images"]  # Ensure images/ contents go to _static/
+html_theme_options = {
+    "logo": {
+        "image_light": "images/DISC-RGB-Icon.svg",
+        "image_dark": "images/DISC-RGB-Icon.svg",
+    },
+    "search_bar_text": "Search this book...",
+    "launch_buttons": {
+        "notebook_interface": "classic",
+        "binderhub_url": "",
+        "jupyterhub_url": "",
+        "thebe": False,
+        "colab_url": "",
+    },
+    "path_to_docs": "unconference",
+    "repository_url": "https://github.com/numfocus/disc-unconference-2025-projects",
+    "repository_branch": "main",
+    "home_page_in_toc": True,
+    "analytics": {"google_analytics_id": ""},
+    "use_repository_button": True,
+    "use_edit_page_button": False,
+    "use_issues_button": True,
+    "article_header_start": ["toggle-primary-sidebar", "version-switcher"],
+    "switcher": {
+        "json_url": "https://raw.githubusercontent.com/numfocus/disc-unconference-2025-projects/main/switcher.json",
+        "version_match": language,
+    },
+    "announcement": "All documents in this repository are in draft status, and work is expected to continue on improvements, additions, and corrections.",
+}
+html_title = "DISC Unconference 2025 Projects"
+latex_engine = "pdflatex"
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",
+    "linkify",
+    "substitution",
+    "tasklist",
+]
+myst_url_schemes = ["mailto", "http", "https"]
+nb_execution_allow_errors = False
+nb_execution_cache_path = ""
+nb_execution_excludepatterns = []
+nb_execution_in_temp = False
+nb_execution_mode = "force"
+nb_execution_timeout = 30
+nb_output_stderr = "show"
+numfig = True
+pygments_style = "sphinx"
+suppress_warnings = ["myst.domains"]
+use_jupyterbook_latex = True
+use_multitoc_numbering = True
---
+++ unconference/intro.md
@@ -0,0 +1,6 @@
+# DISC Unconference 2025 Projects
+
+This website collects all the outputs from the DISC Unconference 2025 in São Paulo, Brazil.
+
+```{tableofcontents}
+```
